e5be8553031f0f7fff80fc1c5420fd32e9949b27
tehhsuhh <38366087+tehhsuhh@users.noreply.github.com>
2019-06-16 21:07:46 -0700

    Update Temperature.java

--- Project2b/src/Temperature.java
+++ Project2b/src/Temperature.java
@@ -1,4 +1,7 @@
-
+//Tessa Biava
+//June 9th, 2019
+//CST-105
+//This is my own work
 import java.util.Scanner;
 
 public class Temperature {
@@ -7,12 +10,16 @@ public class Temperature {
 
 		// prompt user for F input
 		System.out.print("Enter a Fahrenheit temperature: ");
-		double inputF = input.nextDouble();
-
+		double inputF = input.nextDouble();   //reads number from user and assigns
+		
+			//.print prints everything on the same line
+		
 		// calculate F to C
 		double Celsius = (inputF - 32) * 5 / 9;
 		System.out.println(inputF + "F is equivalent to " + Celsius + "C");
-
+		
+			//.println prints things on the next line (layout pleasing)
+		
 		// prompt user for C input
 		System.out.print("Enter a Celsius temperature: ");
 		double inputC = input.nextDouble();
@@ -20,6 +27,7 @@ public class Temperature {
 		// calculate C to F
 		double Fahren = (inputC * (9.0 / 5.0)) + 32;
 		System.out.println(inputC + "C is equivalent to " + Fahren + "F");
+			// the calculations were tricky, parenthesis were KEY!!
 	}
 
 }
